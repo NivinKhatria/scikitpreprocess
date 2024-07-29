2c4423aef1f09aaae650303bf6d7824a5ae2fb8e
NivinKhatria <scartegy2@gmail.com>
2024-07-30 00:42:16 +0530

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,4 @@
+from sklearn.compose import transforms
+from sklearn.preprocess import StandarScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split

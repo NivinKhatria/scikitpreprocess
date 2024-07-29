2bb8b84b6756f9bbaf4e46d60b65997534d186f9
Naren <106871870+narensen@users.noreply.github.com>
2024-07-30 00:44:55 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,4 @@
-from sklearn.compose import transforms
-from sklearn.preprocess import StandarScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandarScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
